91bba75939b2e06c4f14e69cda0d6e51e3970722
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-07-13 23:57:32 +0530

    Revert "feat: add RSS feed for Forklore maintainers and integrate RSS icon in…"
    This reverts commit e604bd971a654ad8a4e94d162ad0950bfb6945b2.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,15 +15,7 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
-      link: [
-        { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
-        {
-          rel: "alternate",
-          type: "application/rss+xml",
-          href: "/rss",
-          title: "Forklore RSS Feed",
-        },
-      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
     },
   },
   routeRules: {
